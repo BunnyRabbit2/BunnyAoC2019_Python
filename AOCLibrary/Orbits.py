c88141a269f5cfac053ab3c1183898287f1b12c4
Lewis Storey (BunnyRabbit2) <iamlewisstorey@yahoo.com>
2020-02-14 22:18:25 +0000

    Almost have day 6 complete Changed over to a dictionary Puzzle 1 answer close but not correct

--- AOCLibrary/Orbits.py
+++ AOCLibrary/Orbits.py
@@ -1,89 +1,50 @@
 class OrbitTreeNode:
-    def __init__(self):
-        self.parentNode = 0
-        self.parentCode = ""
-        self.planetCode = ""
-        self.distanceToRoot = -1
-        self.childNodes = []
-
-    def __init__(self, codeIn, pCodeIn):
-        self.parentNode = 0
+    def __init__(self, codeIn = "", pCodeIn = ""):
         self.parentCode = pCodeIn
         self.planetCode = codeIn
         self.distanceToRoot = -1
+        self.root = True
         self.childNodes = []
 
 class OrbitTree:
     def __init__(self):
-        self.orbits = []
+        self.orbits = {}
 
     def createTree(self, orbitCodes):
-        for o in orbitCodes:
-            addOrbitToTree(o)
+        for parent, child in map(lambda s:s.split(')'), orbitCodes):
+            if parent not in self.orbits:
+                self.orbits[parent] = OrbitTreeNode(parent, "")
+            if child not in self.orbits:
+                self.orbits[child] = OrbitTreeNode(child, parent)
+            else:
+                self.orbits[child].parentCode = parent
 
-        setParentChildRelationships()
+            self.orbits[child].root = False
+            self.orbits[parent].childNodes.append(self.orbits[child])
 
-        setDistancesFromRoot()
+        self.setDistancesFromRoot()
 
-    def addOrbitToTree(self, orbitCode):
-        codes = orbitCode.split(")")
+    def setDistancesFromRoot(self):
+        root = 0
 
-        node = OrbitTreeNode()
-
-        pIndex = -1
-        cIndex = -1
-
-        for i in range(0,len(self.orbits)):
-            if self.orbits[i].planetCode == codes[0]:
-                pIndex = i
-            if self.orbits[i].planetCode == codes[1]:
-                cIndex = i
-
-        if pIndex == -1:
-            self.orbits.append(OrbitTreeNode(parent,""))
-
-        if cIndex != -1:
-            self.orbits[cIndex].parentCode = codes[0]
-        else:
-            self.orbits.append(OrbitTreeNode(codes[1],coes[0]))
-
-    def setParentChildRelationships(self):
-        for otn in self.orbits:
-            pIndex = -1
-
-            for i in range(0,len(self.orbits)):
-                if self.orbits[i].planetCode == otn.parentCode:
-                    pIndex = i
-                    break
-
-            if pIndex != -1:
-                otn.parentNode = self.orbits[pIndex]
-                self.orbits[pIndex].childNodes.append(otn)
-
-    def setDistancesFromRoot(self)
-        rIndex = -1 # root index
-
-        for i in range(0,len(self.orbits)):
-            if self.orbits[i].planetCode == "":
-                rIndex = i
+        for c in self.orbits:
+            if self.orbits[c].root:
+                root = self.orbits[c]
                 break
 
-        root = self.orbits[rIndex]
-        root.distanceToRoot = 0
-
         for c in root.childNodes:
-            setChildrenDistance(c)
+            self.setChildrenDistance(c)
 
     def setChildrenDistance(self, node):
-        node.distanceToRoot = node.parentNode.distanceToRoot + 1
-        for c in root.childNodes:
-            setChildrenDistance(c)
+        node.distanceToRoot = self.orbits[node.parentCode].distanceToRoot + 1
+        for c in node.childNodes:
+            self.setChildrenDistance(c)
 
     def getTotalOrbits(self):
         totalOrbits = 0
 
         for n in self.orbits:
-            totalOrbits += n.distanceToRoot
+            totalOrbits += self.orbits[n].distanceToRoot
 
         return totalOrbits
 
@@ -93,25 +54,25 @@ class OrbitTree:
         start = 0
         end = 0
 
-        for i in range(0,len(self.orbits)):
-            if self.orbits[i].planetCode == startNode:
-                start = self.orbits[i]
+        for o in self.orbits:
+            if o == startNode:
+                start = o
                 break
 
-        for i in range(0,len(self.orbits)):
-            if self.orbits[i].planetCode == endNode:
-                end = self.orbits[i]
+        for o in self.orbits:
+            if o == endNode:
+                end = o
                 break
 
-        startRoute = getRouteToRoot(start)
-        endRoute = getRouteToRoot(end)
+        startRoute = self.getRouteToRoot(self.orbits[start])
+        endRoute = self.getRouteToRoot(self.orbits[end])
 
         matchingNode = 0
         breakLoop = False
 
         for sNode in startRoute:
             for eNode in endRoute:
-                if sNode.planetCode == eNode.planetCode
+                if sNode.planetCode == eNode.planetCode:
                     matchingNode = eNode
                     breakLoop = True
                     break
@@ -120,17 +81,17 @@ class OrbitTree:
 
         if matchingNode.parentCode != 0:
             distance = len(startRoute) + len(endRoute) - matchingNode.distanceToRoot * 2
-            distance -= 2
+            distance -= 4 # remove start, end and the crossover
 
         return distance
 
-        def getRouteToRoot(self, nodeIn):
-            route = []
+    def getRouteToRoot(self, nodeIn):
+        route = []
 
-            currentNode = nodeIn
+        currentNode = nodeIn
 
-            while currentNode.parentCode != "":
-                route.append(currentNode)
-                currentNode = currentNode.parentNode
+        while not currentNode.root:
+            route.append(currentNode)
+            currentNode = self.orbits[currentNode.parentCode]
 
-            return route
+        return route
--- DayCode/Day6.py
+++ DayCode/Day6.py
@@ -1,18 +1,34 @@
 import pathlib
+import re
+from AOCLibrary import Orbits
 
 def loadInputs(fileLocation):
+    lines = []
+
     file = pathlib.Path(fileLocation)
     if file.exists():
-        return file.read_text().split(",")
+        with file.open() as f:
+            lines = f.read().splitlines()
+            return lines
     else:
         print("Day 6 input file does not exist")
 
 def solvePuzzle1(fileLocation):
     result = 0
 
-    print("Day 5 Puzzle 1 solution - " + str(result))
+    oTree = Orbits.OrbitTree()
+    oTree.createTree(loadInputs(fileLocation))
+
+    result = oTree.getTotalOrbits()
+
+    print("Day 6 Puzzle 1 solution - " + str(result))
 
 def solvePuzzle2(fileLocation):
     result = 0
+
+    oTree = Orbits.OrbitTree()
+    oTree.createTree(loadInputs(fileLocation))
+
+    result = oTree.getDistanceBetweenTwoOrbits("YOU","SAN")
     
     print("Day 6 Puzzle 2 solution - " + str(result))
